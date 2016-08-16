e977c25e0b808371380fa390fdb484e13c4fd959
shaktech786 <shakeel.bhamani@gmail.com>
2016-08-16 14:08:17 -0500

    added random helloworld stuff to test

--- src/main/java/com/springapp/mvc/HelloController.java
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -11,6 +11,8 @@ public class HelloController {
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
+		model.addAttribute("prompt", "What's your name?");
+		model.addAttribute("name", "Shakeel");
 		return "hello";
 	}
 }
--- src/main/webapp/WEB-INF/pages/hello.jsp
+++ src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,5 +1,7 @@
 <html>
 <body>
 	<h1>${message}</h1>
+	<h2>${prompt}</h2>
+	<h3>${name}</h3>
 </body>
 </html>
