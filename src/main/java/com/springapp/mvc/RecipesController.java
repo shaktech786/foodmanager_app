9e264693fa4ab018084c8ca4a9fbbc755e00be0a
Nicolette Chambers <nicolette3883@gmail.com>
2016-08-18 14:03:59 -0500

    Create RecipesController and add tests

--- src/main/java/com/springapp/mvc/RecipesController.java
+++ src/main/java/com/springapp/mvc/RecipesController.java
@@ -1,8 +1,10 @@
 package com.springapp.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/recipes")
@@ -10,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class RecipesController {
 
     @RequestMapping(method = RequestMethod.POST)
-    public String displayRecipes() {
+    public String displayRecipes(@RequestParam String ingredients, ModelMap modelMap) {
+        modelMap.addAttribute("ingredientList", ingredients);
         return "recipes";
     }
 }
--- src/test/java/com/springapp/mvc/RecipesControllerTest.java
+++ src/test/java/com/springapp/mvc/RecipesControllerTest.java
@@ -1,21 +1,46 @@
 package com.springapp.mvc;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.ui.ModelMap;
 
+import static org.hamcrest.Matchers.hasEntry;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/mvc-dispatcher-servlet.xml")
 public class RecipesControllerTest {
 
-    @Test
-     public void shouldDisplayRecipesPageWhenRootRouteIsHit() {
-        RecipesController recipesController = new RecipesController();
 
-        assertThat(recipesController.displayRecipes(), is("recipes"));
+    private ModelMap modelMap;
+    private String ingredients;
+    private RecipesController recipesController;
+
+    @Before
+    public void setUp() throws Exception {
+        modelMap = new ModelMap();
+        ingredients = "apples,oranges";
+        recipesController = new RecipesController();
     }
+
+    @Test
+    public void shouldReturnRecipesWhenRootRouteIsHit() {
+        assertThat(recipesController.displayRecipes(ingredients, modelMap), is("recipes"));
+    }
+
+    @Test
+     public void shouldContainCorrectKeyAndValueInModelMapWhenDisplayRecipesIsCalled() {
+        recipesController.displayRecipes(ingredients, modelMap);
+
+        assertThat(modelMap, hasEntry("ingredientList", (Object) ingredients));
+    }
+    
+    
+
+
+
 }
