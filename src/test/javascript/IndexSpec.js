9888cbd14ec9d588f239641614fe1117f270459d
Nicolette Chambers <nicolette3883@gmail.com>
2016-08-18 15:20:06 -0500

    Add tests to IndexSpec.js

--- src/test/javascript/IndexSpec.js
+++ src/test/javascript/IndexSpec.js
@@ -1,8 +1,49 @@
 describe('Index', function(){
-	it('should call validateInput when submit button is clicked', function () {
-		spyOn(window, 'validateInput');
-		$("#submit").onclick();
 
-		expect(window.validateInput()).toHaveBeenCalled();
-	})
+    beforeAll(function() {
+        var elements = [];
+        elements.push("<p id='ingredient-error-message' style='display:none;'></p>");
+        elements.push("<textarea style='display:none;'></textarea>");
+        for (var i = 0; i < elements.length; i++) {
+            $(document.body).append(elements[i]);
+        }
+    });
+
+	it('should Display Error Message When One Of The Ingredients Contains A Symbol', function () {
+       var ingredient =  $('textarea').text("apple, pear%%");
+
+		validateInput();
+
+		expect($('#ingredient-error-message').text()).toBe("The following errors must be corrected: pear%%");
+	});
+
+    it('should Display Error Message When One Of The Ingredients Contains A Number', function () {
+        var ingredient =  $('textarea').text("apple, pear1");
+
+        validateInput();
+
+        expect($('#ingredient-error-message').text()).toBe("The following errors must be corrected: pear1");
+    });
+
+    it('should Display Error Message To Page When One Of The Ingredients Contains Numbers And Symbols', function () {
+        var ingredient =  $('textarea').text("apple, pear123$$");
+
+        validateInput();
+
+        expect($('#ingredient-error-message').text()).toBe("The following errors must be corrected: pear123$$");
+    });
+
+    it('should Display Error Message To Page When Multiple Ingredients Contains Numbers And Symbols', function () {
+        var ingredient =  $('textarea').text("apple, pear$$, peach123");
+
+        validateInput();
+
+        expect($('#ingredient-error-message').text()).toBe("The following errors must be corrected: pear$$,peach123");
+    });
+
+    it('should Return True When The Ingredients Entered Are Valid', function () {
+        $('textarea').text("apple, pear");
+
+        expect(validateInput()).toBe(true);
+    })
 });
